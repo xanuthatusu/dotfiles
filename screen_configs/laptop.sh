a4b77b4d711c63af50607f2ba203d80fcbe5f723
Xanuthatusu <anthony@tepia.net>
2018-08-09 12:26:45 -0600

    Update screen_configs to work with the usb-c ports

--- screen_configs/laptop.sh
+++ screen_configs/laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP1 --mode 1920x1080 --primary --output HDMI1 --off
+xrandr --output eDP1 --mode 1920x1080 --primary --output DP1 --off
--- screen_configs/work.sh
+++ screen_configs/work.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-xrandr --output HDMI1 --mode 1920x1080 --primary --output eDP1 --mode 1920x1080 --right-of HDMI1
+#xrandr --output HDMI1 --mode 1920x1080 --primary --output eDP1 --mode 1920x1080 --right-of HDMI1
+xrandr --output HDMI1 --off --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --off
+
