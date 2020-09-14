e777405ca5c1c2fefd57f080556a91374518c9bc
Anthony George <anthony.george@tcnbroadcasting.com>
2020-09-14 14:33:19 -0600

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,13 @@
 (recentf-mode 1)
 (setq-default recent-save-file "~/.emacs.d/recentf")
 
+(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
 ;; Package configs
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -134,6 +141,8 @@
    :states 'visual
    :prefix "SPC"
    "c" 'comment-dwim))
+
+(global-set-key (kbd "s-s") 'save-buffer)
 
 (use-package magit
   :ensure t)
